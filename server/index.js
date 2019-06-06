4a846fab178c50e505bf5b0ed6f1bc7ecffb8138
Gordana Nikolic <gordana.nikolic6@gmail.com>
2019-06-06 19:45:01 +0100

    register, login and auth middleware on server added

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const config = require("./config/dev");
 const Rental = require("./models/rental");
 const FakeDb = require("./fake-db");
 const rentalRoutes = require("./routes/rentals");
+const userRoutes = require("./routes/users");
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true }).then(() => {
   const fakeDb = new FakeDb();
-  fakeDb.pushRentalsToDb();
+  fakeDb.pushDataToDb();
 });
+mongoose.set("useCreateIndex", true); //prevent warning
+
 const app = express();
+app.use(bodyParser.json());
 app.use("/api/v1/rentals", rentalRoutes);
+app.use("/api/v1/users", userRoutes);
 
 const PORT = process.env.port || "3001";
 app.listen("3001", () => {
--- server/routes/rentals.js
+++ server/routes/rentals.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const Rental = require("../models/rental");
+const UserCtrl = require("../controllers/user");
 
 const router = express.Router();
+
+router.get("/secret", UserCtrl.authMiddleware, (req, res) => {
+  res.json({ secret: true });
+});
 router.get("", (req, res) => {
   Rental.find({}, (err, foundRentals) => {
     res.json(foundRentals);
@@ -12,11 +17,9 @@ router.get("/:id", (req, res) => {
   const rentalId = req.params.id;
   Rental.findById(rentalId, (err, foundRentals) => {
     if (err) {
-      res
-        .status(422)
-        .send({
-          errors: [{ title: "Rental Error", detail: "Could not find rental" }]
-        });
+      res.status(422).send({
+        errors: [{ title: "Rental Error", detail: "Could not find rental" }]
+      });
     }
     res.json(foundRentals);
   });
